b66406c2d628cfc11b301aae13b8c8aa56031575
anamsood <anam1634.be21@chitkara.edu.in>
2024-03-19 23:02:04 +0530

    client setup

---
+++ Client/src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+	return (
+		<>
+			<div id="nav">
+				<p>logo</p>
+				<p>site name</p>
+				<p>location</p>
+				<p>search</p>
+				<p>contact us</p>
+				<p>account</p>
+			</div>
+		</>
+	);
+}
